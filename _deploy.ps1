03f94ab2d9fdc49b5a51c9634d1b5769e9dc9170
akshayraj-1 <akshayraj4101@gmail.com>
2024-06-28 11:03:57 +0530

    Auto

--- _deploy.ps1
+++ _deploy.ps1
@@ -1,3 +1,18 @@
+# Step 1: Read the current version from package.json
+$packageJsonPath = ".\frontend\package.json"
+$packageJson = Get-Content $packageJsonPath -Raw | ConvertFrom-Json
+$currentVersion = $packageJson.version
+Write-Host "Current Version: $currentVersion"
+
+# Step 2: Prompt the user to input the new version
+$newVersion = Read-Host "Enter new version"
+
+# Step 3: Update the package.json with the new version
+$packageJson.version = $newVersion
+$packageJson | ConvertTo-Json -Depth 32 | Set-Content $packageJsonPath
+Write-Host "Updated Version: $newVersion"
+
+# Step 4: Build and Deploy
 Write-Host "Building..."
 Set-Location ".\frontend\"
 npm run build
@@ -6,4 +21,6 @@ Write-Host "============================="
 Write-Host "Deploying..."
 firebase deploy
 Write-Host "Deployed to the server"
+
+# Step 5: Return to the original directory
 Set-Location "C:\Users\aksha\Projects\React\byte-threads\"
