d7fe41a355bab46c14a4b475318dae46f8deeb80
akshayraj-1 <akshayraj4101@gmail.com>
2024-07-24 20:32:05 +0530

    Modified Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,16 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import cn from "../utils/cn.util.js";
 
 function Navbar(props) {
-  return (
-      // eslint-disable-next-line react/prop-types
-    <nav className={cn("fixed top-0 left-0 z-[45] flex justify-between w-full px-5 sm:px-9 py-5 backdrop-blur-sm", props.className)}>
-    <Link to="/" className="text-lg font-normal">
-    <span className="text-white">qwik</span>
-    <span className="text-white">chat</span>
-    </Link>
-    </nav>
-  )
+    return (
+        <nav
+            className={cn("fixed top-0 left-0 z-[45] flex justify-between w-full px-5 sm:px-9 py-[1.3rem] backdrop-blur-sm", props.className)}>
+            <Link to="/" className="text-[1.15rem] font-normal">
+                <span className="text-white">qwik</span>
+                <span className="text-white">chat</span>
+            </Link>
+        </nav>
+    )
 }
 
 export default Navbar;
