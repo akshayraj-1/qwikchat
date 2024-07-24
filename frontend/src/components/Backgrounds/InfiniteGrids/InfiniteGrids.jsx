e9f39e0884a17bfdf7419d1562eaa6f9f52b849a
akshayraj-1 <akshayraj4101@gmail.com>
2024-07-24 20:32:05 +0530

    Modified InfiniteGrids.jsx

--- frontend/src/components/Backgrounds/InfiniteGrids/InfiniteGrids.jsx
+++ frontend/src/components/Backgrounds/InfiniteGrids/InfiniteGrids.jsx
@@ -4,9 +4,8 @@ import cn from "../../../utils/cn.util.js";
 function InfiniteGrids({children, ...props}) {
     return (
         <>
-            <div id="infinite-grids" className={cn("fixed h-full w-full", props.className)}>
-            </div>
-            <div className="relative z-[2] h-full w-full bg-gradient-to-t from-primary/80 to-transparent">
+            <div id="infinite-grids" className="fixed size-full" style={{ opacity: props.opacity }}/>
+            <div className={cn("relative z-[2] size-full bg-gradient-to-t from-primary/80 to-transparent", props.className)}>
                 {children}
             </div>
         </>
