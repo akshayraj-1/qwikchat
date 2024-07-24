c2e40fd6c3f9c5f5daa6259bb6c136ffd99c2db6
akshayraj-1 <akshayraj4101@gmail.com>
2024-07-24 20:32:05 +0530

    Modified MeshGradient.jsx

--- frontend/src/components/Backgrounds/MeshGradient.jsx
+++ frontend/src/components/Backgrounds/MeshGradient.jsx
@@ -1,15 +1,17 @@
 import cn from "../../utils/cn.util.js";
 
-// eslint-disable-next-line react/prop-types
 function MeshGradient({children, ...props}) {
     return (
-        <div className="relative h-full w-full overflow-hidden">
-            <div className="absolute -bottom-36 left-0 h-[80vh] w-full bg-glow bg-cover bg-bottom bg-no-repeat blur-3xl"
-                 style={{opacity: props.opacity}}/>
-            <div className={cn("relative z-[1]", props.className)}>
-            {children}
+        <>
+            <div className="relative size-full overflow-hidden">
+                <div
+                    className="absolute -bottom-10 left-0 right-0 mx-auto h-[85vh] w-full md:w-[95vw] bg-glow bg-cover bg-bottom bg-no-repeat blur-3xl"
+                    style={{opacity: props.opacity}}/>
+                <div className={cn("relative z-[2]", props.className)}>
+                    {children}
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
