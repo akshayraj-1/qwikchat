f7a7c8b91c9e6ea55810e5f87f0f7aff1d414e4f
akshayraj-1 <akshayraj4101@gmail.com>
2024-08-03 17:53:51 +0530

    Modified MeshGradient.jsx

--- frontend/src/components/Backgrounds/MeshGradient.jsx
+++ frontend/src/components/Backgrounds/MeshGradient.jsx
@@ -1,12 +1,13 @@
 import cn from "../../utils/cn.util.js";
+import glow2 from "../../assets/images/glow2.png";
 
 function MeshGradient({children, ...props}) {
     return (
         <>
             <div className="relative size-full overflow-hidden">
                 <div
-                    className="absolute -bottom-10 left-0 right-0 mx-auto h-[85vh] w-full md:w-[95vw] bg-glow bg-cover bg-bottom bg-no-repeat blur-3xl"
-                    style={{opacity: props.opacity}}/>
+                    className="absolute -bottom-14 left-0 right-0 mx-auto h-[75vh] w-full bg-cover bg-bottom bg-no-repeat blur-3xl"
+                    style={{opacity: props.opacity, backgroundImage: `url(${glow2})`}}/>
                 <div className={cn("relative z-[2]", props.className)}>
                     {children}
                 </div>
