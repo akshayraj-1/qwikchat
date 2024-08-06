69576ff6ab44784b9c3ce384adf8b809de90c741
akshayraj-1 <akshayraj4101@gmail.com>
2024-08-06 23:08:22 +0530

    Modified MessageFormatter.jsx

--- frontend/src/components/Chat/MessageFormatter.jsx
+++ frontend/src/components/Chat/MessageFormatter.jsx
@@ -5,29 +5,29 @@ import cn from "../../utils/cn.util.js";
 import {detectLanguage} from "../../utils/text.util.js";
 import CustomSyntaxStyle from "../../values/custom-syntax-style.json";
 
-function MessageFormatter({ message, onClick }) {
+function MessageFormatter({message, onClick}) {
 
     const parts = message.split('```');
-    const hasOnlyCode = parts.filter((part, index) => index % 2 === 0 && part.trim() !== '').length === 0;
 
     const urlRegex = /(https?:\/\/[^\s]+)/g;
-    const renderTextWithLinks = (text) =>
-        text.split(urlRegex).map((part, index) =>
+    const renderTextWithLinks = (text) => {
+        return text.split(urlRegex).map((part, index) =>
             index % 2 === 0 ? (
-                <p key={uuidv4()} className="inline-block tracking-normal break-words hyphens-auto"
-                   style={{wordBreak: 'break-word'}}>
+                <p key={uuidv4()} className="inline-block tracking-normal break-words hyphens-auto">
                     {part}
                 </p>
             ) : (
                 <a
                     key={uuidv4()} href={part} target="_blank" rel="noopener noreferrer"
-                    className="tracking-normal break-words text-blue-400 cursor-pointer hover:underline">
+                    className="tracking-normal break-words text-accent cursor-pointer hover:underline">
                     {part}
                 </a>
             )
         );
+    };
 
-    const renderedParts = parts.map((part, index) => {
+
+    const formattedParts = parts.map((part, index) => {
         if (index % 2 === 1) {
             return (
                 <SyntaxHighlighter key={uuidv4()}
@@ -35,7 +35,7 @@ function MessageFormatter({ message, onClick }) {
                                    style={CustomSyntaxStyle}
                                    wrapLongLines={true}
                                    title={"Click to copy the code"}
-                                   className={cn(hasOnlyCode ? "rounded-md" : "rounded-md my-2", "cursor-pointer break-words whitespace-pre", "tracking-normal")}
+                                   className={cn("cursor-pointer break-words whitespace-pre tracking-normal")}
                                    onClick={onClick}
                 >
                     {part.trim()}
@@ -46,7 +46,7 @@ function MessageFormatter({ message, onClick }) {
         }
     });
 
-    return <div>{renderedParts}</div>;
+    return <div>{formattedParts}</div>;
 }
 
 export default MessageFormatter;
