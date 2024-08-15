b8c2ac51368689820142eefa1a48587aca43ae85
akshayraj-1 <akshayraj4101@gmail.com>
2024-08-15 12:59:04 +0530

    Modified Navbar.jsx

--- frontend/src/components/UI/Navbar.jsx
+++ frontend/src/components/UI/Navbar.jsx
@@ -7,9 +7,8 @@ function Navbar(props) {
         <nav
             className={cn("fixed top-0 left-0 z-[45] flex justify-between w-full px-5 sm:px-9 py-[1.3rem] backdrop-blur-sm", props.className)}>
             <Link to="/" className="flex items-center justify-center text-[1.15rem] font-normal text-textPrimary">
-                <object className="h-5 me-2 -mb-[1px] " data={qwikchat_logo} type="image/svg+xml" />
-                <span>qwik</span>
-                <span>chat</span>
+                <img className="h-5 me-2 -mb-[1px] " src={qwikchat_logo} alt="qwikchat"/>
+                <span>qwikchat</span>
             </Link>
         </nav>
     )
