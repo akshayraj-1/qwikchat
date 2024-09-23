5ee9d4795d7e9c726c6cc531c33280d380f587fa
akshayraj-1 <akshayraj4101@gmail.com>
2024-09-23 14:29:54 +0530

    Deleted InfiniteGrids.jsx

---
+++ frontend/src/components/Backgrounds/InfiniteGrids/InfiniteGrids.jsx
@@ -0,0 +1,22 @@
+import "./InfiniteGrids.scss";
+import PropTypes from "prop-types";
+import cn from "../../../utils/cn.util.js";
+
+function InfiniteGrids({children, ...props}) {
+    return (
+        <>
+            <div id="infinite-grids" className="fixed size-full" style={{ opacity: props.opacity }}/>
+            <div className={cn("relative z-[2] size-full bg-gradient-to-t from-primary/80 to-transparent", props.className)}>
+                {children}
+            </div>
+        </>
+    )
+}
+
+InfiniteGrids.propTypes = {
+    children: PropTypes.node.isRequired,
+    opacity: PropTypes.number,
+    className: PropTypes.string
+};
+
+export default InfiniteGrids;
