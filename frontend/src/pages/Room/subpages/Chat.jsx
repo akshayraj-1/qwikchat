a21de8c9a4ccdcfe0f2ac91373b7848002f74b94
akshayraj-1 <akshayraj4101@gmail.com>
2024-10-11 14:29:52 +0530

    Modified Chat.jsx

--- frontend/src/pages/Room/subpages/Chat.jsx
+++ frontend/src/pages/Room/subpages/Chat.jsx
@@ -113,7 +113,7 @@ function Chat({ socket }) {
                     }
                 </div>
                 <div
-                    className="flex items-center gap-3 w-full mb-4 mt-2 p-2 text-sm rounded-xl border border-secondaryLight bg-secondaryVariant">
+                    className="flex items-center gap-3 w-full mb-4 mt-2 p-2 text-sm rounded-xl border border-surfaceLight bg-surfaceVariant">
 
                     <input ref={pickerRef} hidden={true} type="file"
                            accept={"image/png, image/jpg, image/jpeg, image/gif"} onChange={sendImageMessage}/>
@@ -142,7 +142,7 @@ function Chat({ socket }) {
                         }}
                     />
                     <div
-                        className={cn("bg-secondaryLight p-2 rounded-full flex items-center justify-center self-end", inputMessage?.length > 0 ? "cursor-pointer text-primaryText bg-gradient-to-br from-accent to-accentDark transition-all duration-100" : "text-textSecondary cursor-default")}
+                        className={cn("bg-surfaceLight p-2 rounded-full flex items-center justify-center self-end", inputMessage?.length > 0 ? "cursor-pointer text-primaryText bg-gradient-to-br from-primary to-primaryDark transition-all duration-100" : "text-textSecondary cursor-default")}
                         onClick={(e) => {
                             e.stopPropagation();
                             e.preventDefault();
