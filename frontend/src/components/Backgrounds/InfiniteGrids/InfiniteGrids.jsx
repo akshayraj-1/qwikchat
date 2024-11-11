10263c26ba87ec8b02dbc61bf1c7b84167364a69
akshayraj-1 <akshayraj4101@gmail.com>
2024-11-11 23:41:55 +0530

    Changed color names

--- frontend/src/components/Backgrounds/InfiniteGrids/InfiniteGrids.jsx
+++ frontend/src/components/Backgrounds/InfiniteGrids/InfiniteGrids.jsx
@@ -6,7 +6,7 @@ function InfiniteGrids({children, ...props}) {
     return (
         <>
             <div id="infinite-grids" className="fixed size-full" style={{ opacity: props.opacity }}/>
-            <div className={cn("relative z-[2] size-full bg-gradient-to-t from-background/80 to-transparent", props.className)}>
+            <div className={cn("relative z-[2] size-full bg-gradient-to-t from-colorBackground/80 to-transparent", props.className)}>
                 {children}
             </div>
         </>
